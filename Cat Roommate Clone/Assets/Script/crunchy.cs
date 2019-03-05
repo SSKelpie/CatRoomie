59a780d151342b9998aa1722bfd37c7f899d62e4
Gabi Dias <Gabi7778@hotmail.com>
2019-03-05 10:07:24 -0500

    Starting Point

---
+++ Cat Roommate Clone/Assets/Script/crunchy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class crunchy : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public string[] textstuff;
+
+    public void ChangeText()
+    {
+        int randnum;
+        randnum = Random.Range(0, textstuff.Length);
+        text.text = textstuff[randnum];
+    }
+}
