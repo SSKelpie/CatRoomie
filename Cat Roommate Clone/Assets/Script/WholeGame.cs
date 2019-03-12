7d3624f181bf42fff421a95ff1faa11325047568
Gabi Dias <Gabi7778@hotmail.com>
2019-03-12 00:38:08 -0400

    Changed Pathways to use UI Button On.Click, Works ok
    Fixed some images and arranged code. Commented parts out for later use.
    Used On.Click and the _answerstate for answer keys.

--- Cat Roommate Clone/Assets/Script/WholeGame.cs
+++ Cat Roommate Clone/Assets/Script/WholeGame.cs
@@ -10,14 +10,17 @@ using UnityEngine.UI;
 
 public class WholeGame : MonoBehaviour
 {
-    int _answerstate = 0;
+    public int _answerstate = 0;
     public int _goodanswer = 0;
     public int _badanswer = 0;
-    public TextMeshProUGUI leftanswer;
+    public TextMeshProUGUI leftanswer;        
     public TextMeshProUGUI rightanswer;
-    public Button leftbut;
-    public Button rightbut;
-    public bool isSelect;
+    public Button b1; // left button
+    public Button b2; // right button
+    
+    //right so these were calling buttons but didn't use UI or onClick
+    //public Button rightbut;
+    //public Button leftbut;
 
     public TextMeshProUGUI speech;
     public Sprite[] cat;
@@ -26,7 +29,9 @@ public class WholeGame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        b1.onClick.AddListener(ClickHandlerRightButton);
+        b2.onClick.AddListener(ClickHandlerLeftButton);
+       // ButtonChoices();
     }
 
     // Update is called once per frame
@@ -40,11 +45,8 @@ public class WholeGame : MonoBehaviour
             rightanswer.text = "Early Bird!";
             catsprite.sprite = cat[10];
             _answerstate++;
-            
-            if (rightbut)
-            {
-                isSelect = !isSelect;
-            }
+
+
 
         }
         else if (_answerstate == 1)
@@ -52,110 +54,148 @@ public class WholeGame : MonoBehaviour
             speech.text = "What do you like on your pizza?";
             leftanswer.text = "Gotta love greens";
             rightanswer.text = "I'm all about anchovies";
+            catsprite.sprite = cat[4];
             _answerstate++;
-
-            if (isSelect)
-            {
-                _goodanswer++;
-                catsprite.sprite = cat[3];
-            }
-            if (!isSelect)
-            {
-                _badanswer++;
-                catsprite.sprite = cat[11];
-               
-            }
+            //failed attempts
+//            if (b1)
+//            {
+//                ClickHandlerCorrect();
+//            }
+//
+//            if (b2)
+//            {
+//                ClickHandlerWrong();
+//            }
+            
             
         }
         else if (_answerstate == 2)
         {
             speech.text = "Are you allergic to cats?";
-            leftanswer.text = "Yes dude, get away!";
-            rightanswer.text = "Nah.";
+            leftanswer.text = "Nah.";
+            rightanswer.text = "Yes dude, get away!";
+            catsprite.sprite = cat[9];
             _answerstate++;
-
-            if (leftbut)
-            {
-                _badanswer++;
-                catsprite.sprite = cat[9];
-            }
-            else if (rightbut)
-            {
-                _goodanswer++;
-                catsprite.sprite = cat[3];
-            }
+//old code left in to remind me what not to do
+//            if (leftbut)
+//            {
+//                _badanswer++;
+//                catsprite.sprite = cat[9];
+//            }
+//            else if (rightbut)
+//            {
+//                _goodanswer++;
+//                catsprite.sprite = cat[3];
+//            }
         }
         else if (_answerstate == 3)
         {
             speech.text = "How do you feel about naps?";
-            leftanswer.text = "Love 'em!";
-            rightanswer.text = "Hate 'em.";
+            leftanswer.text = "Hate 'em.";
+            rightanswer.text = "Love 'em!";
+            catsprite.sprite = cat[6];
             _answerstate++;
 
-            if (leftbut)
-            {
-                _badanswer++;
-                catsprite.sprite = cat[1];
-            }
-            else if (rightbut)
-            {
-                _goodanswer++;
-                catsprite.sprite = cat[3];
-            }
+//            if (leftbut)
+//            {
+//                _badanswer++;
+//                catsprite.sprite = cat[1];
+//            }
+//            else if (rightbut)
+//            {
+//                _goodanswer++;
+//                catsprite.sprite = cat[3];
+//            }
         }
         else if (_answerstate == 4)
         {
             speech.text = "Don't you hate birds?";
             leftanswer.text = "They're the worst!";
             rightanswer.text = "Not really man.";
+            catsprite.sprite = cat[3];
             _answerstate++;
-            if (leftbut)
-            {
-                _goodanswer++;
-                catsprite.sprite = cat[3];
-            }
-            else if (rightbut)
-            {
-                _badanswer++;
-                catsprite.sprite = cat[3];
-            }
+//            if (leftbut)
+//            {
+//                _goodanswer++;
+//                catsprite.sprite = cat[3];
+//            }
+//            else if (rightbut)
+//            {
+//                _badanswer++;
+//                catsprite.sprite = cat[3];
+//            }
         }
         else if (_answerstate == 5)
         {
             speech.text = "Alright, can I have some belly rubs?";
-            leftanswer.text = "I dont know...";
-            rightanswer.text = "Sure thing!";
+            leftanswer.text = "Sure Thing!";
+            rightanswer.text = "I dont know...";
+            catsprite.sprite = cat[7];
             _answerstate++;
 
-            if (leftbut)
-            {
-                _goodanswer++;
-                catsprite.sprite = cat[3];
-            }
-            else if (rightbut)
-            {
-                _badanswer++;
-                catsprite.sprite = cat[6];
-            }
+//            if (leftbut)
+//            {
+//                _goodanswer++;
+//                catsprite.sprite = cat[3];
+//            }
+//            else if (rightbut)
+//            {
+//                _badanswer++;
+//                catsprite.sprite = cat[6];
+//            }
         }
         else if (_answerstate == 6)
         {
             ChangeSpeech();
-            if (leftbut)
-            {
-                _badanswer++;
-            }
-            else if (rightbut)
-            {
-                _goodanswer++;
-            }
         }
     }
 
-    void ClickSelect()
+
+    void ClickHandlerLeftButton()
     {
-        isSelect = !isSelect;
+        if (_answerstate % 2 == 0)
+        {
+            _badanswer++;
+        }
+        else
+        {
+            _goodanswer++;
+        }
+        // do the things you want it to do
     }
+
+    void ClickHandlerRightButton()
+    { 
+        if (_answerstate % 2 == 0)
+        {
+            _goodanswer++;
+        }
+        else
+        {
+            _badanswer++;
+        }
+
+    }
+
+    void ButtonChoices()
+    {
+        //switch added listener each time it was called
+//        switch (_answerstate % 2)
+//        {
+//            case 0:
+//                b1.onClick.Invoke();
+//                b2.onClick.Invoke();
+//                break;
+//            case 1:
+//                b2.onClick.AddListener(ClickHandlerCorrect);
+//                b1.onClick.AddListener(ClickHandlerWrong);
+//                break;
+//        }
+        b1.onClick.Invoke();
+        b2.onClick.Invoke();
+        
+        
+    } 
 
 
     void ChangeSpeech()
